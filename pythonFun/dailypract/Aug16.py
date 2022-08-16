01dc805dbb66bdb0d65ea420eecab7d8954cd428
dharshinik04 <dharshinik04@gmail.com>
2022-08-16 14:53:58 +0530

    workouts

---
+++ pythonFun/dailypract/Aug16.py
@@ -0,0 +1,4 @@
+#given an array of inters num and an inter target,
+#return the indices of the two number such that they add up to target
+from typing import List
+#num = [1,2,3,4,5,6] , target = 9 -> [3,4]
